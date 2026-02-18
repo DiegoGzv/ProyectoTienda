34385be9affb16935a29a0556394a2560c70c050
Diego González Villasmil <74458156+DiegoGzv@users.noreply.github.com>
2026-02-18 15:25:02 -0500

    Update Main.java

--- ProyectoTienda/src/tienda/app/Main.java
+++ ProyectoTienda/src/tienda/app/Main.java
@@ -8,12 +8,18 @@ public class Main {
 
         Producto p1 = new Producto("Laptop", 2500000);
         Producto p2 = new Producto("Mouse", 50000);
+        Producto p3 = new Producto("Teclado", 80000);
 
         CarritoCompra carrito = new CarritoCompra("Diego");
 
         carrito.añadirProducto(p1);
         carrito.añadirProducto(p2);
+        carrito.añadirProducto(p3);
 
+        // El usuario decide quitar el mouse
+        carrito.eliminarProducto(p2);
+
+        // Ahora sí genera la factura
         Factura factura = carrito.generarFactura("F001");
 
         System.out.println("Subtotal: $" + factura.calcularSubtotal());
